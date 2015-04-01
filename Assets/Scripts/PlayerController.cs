be81c438d4bddbcc4262fb5ceda93ed51d5266a7
Tools hC <hc.digitaltools@gmail.com>
2015-04-01 17:09:40 -0500

    Adds player controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		GetComponent<Rigidbody>().AddForce (movement * speed);
+
+		GetComponent<Rigidbody>().position = new Vector3 (
+
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+
+		);
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		
+	}
+	
+	void FixUpdate( ){
+		
+		
+		
+	}
+
+}
